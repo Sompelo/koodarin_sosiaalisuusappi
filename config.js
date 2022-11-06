24ce0035eb6a6e8510b14484c6caf6710ca8a3bb
Samuli Salmi <91623479+Sompelo@users.noreply.github.com>
2022-11-06 20:20:11 +0200

    Update config.js

--- config.js
+++ config.js
@@ -4,12 +4,6 @@ import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, on
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA5SUkxCdGVb6EoWg8YBdMdlM7fx1Hq5UI",
-  authDomain: "koodarin-sosiaalisuusappi.firebaseapp.com",
-  projectId: "koodarin-sosiaalisuusappi",
-  storageBucket: "koodarin-sosiaalisuusappi.appspot.com",
-  messagingSenderId: "1005447914179",
-  appId: "1:1005447914179:web:246f4275a28758b2c4b876"
 };
 
 
